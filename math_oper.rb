e4b26954fe7d70e0053abf888800d242c389e568
Sindhussn <30837243+Sindhussn@users.noreply.github.com>
2017-12-07 16:56:33 +0530

    Create math_oper.rb

---
+++ math_oper.rb
@@ -0,0 +1,14 @@
+a = 2
+b = 3
+add = a+b
+sub = b-a
+mul = a*b
+div = b/a
+exp = a**b
+mod = b%a
+puts add    #  5
+puts sub    #  1
+puts mul    #  6
+puts div    #  1
+puts exp    #  8
+puts mod    #  1
