eeb6ff593c539a06cba9d40eb2f219f0834c5bf8
Sindhussn <30837243+Sindhussn@users.noreply.github.com>
2017-12-11 15:33:57 +0530

    Create truthvalue.rb
    The values that are considered true and the values that are false

---
+++ truthvalue.rb
@@ -0,0 +1,6 @@
+if 0                                              # 1 0 and "" is considered as true
+  puts "Hey, 0 is considered to be a truth in Ruby" 
+end
+if !nill                                          # false can be used instead of nill
+  puts "nill is considered to be false in Ruby"
+end
