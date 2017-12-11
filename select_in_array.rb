5289979494c83e162e646c8457b9cfe20326f7e5
Sindhussn <30837243+Sindhussn@users.noreply.github.com>
2017-12-11 15:51:08 +0530

    Create select_in_array.rb

---
+++ select_in_array.rb
@@ -0,0 +1,3 @@
+names = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+names.select{ |string| string.length > 5}                 #select method is used to select the elements from 
+                                                                                  # the array based on given condition
