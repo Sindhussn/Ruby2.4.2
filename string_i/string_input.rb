62a4593dcf2768ad009c3cd67bb4518c66add37a
Sindhussn <30837243+Sindhussn@users.noreply.github.com>
2017-12-15 11:54:03 +0530

    Create string_input.rb

---
+++ string_i/string_input.rb
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'Github '
+# Declare second integer, double, and String variables.
+j = gets.chomp.to_i                                          #gets an input without newline character and converts to integer
+e = gets.chomp.to_f                                          #gets an input without newline character and converts to float
+str = gets.chomp
+# Read and save an integer, double, and String to your variables.
+puts i+j
+# Print the sum of both integer variables on a new line.
+puts d+e
+# Print the sum of the double variables on a new line.
+puts s+str
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
